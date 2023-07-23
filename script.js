4e0f36215c94d736096f96b87439814204a6030c
archanak55201 <55021100+archanak55201@users.noreply.github.com>
2023-07-23 17:37:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const ilevel=document.getElementsByTagName("li");
+// console.log(level[4].id);
+const n=ilevel.length 
+for(let i=0;i<n;i++){
+    if(ilevel[i].id==="level"){
+        i++;
+        alert("The level of the element is: "+i);
+   
+    }
+}
